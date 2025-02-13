19d61dab69216bb4bf4b0993df0c0e360f72d3a5
ajc32199 <adam.chiarella@gmail.com>
2025-02-13 11:23:43 -0600

    added basic buttons while playing around. Still needs UI Homepage overhaul.

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import AppbarHeader from './Appbar';
+import { Button, SafeAreaView } from 'react-native-web';
 
 export default function App() {
   return (
-    <View>
-      <Text>Title</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>
+        Let's start this test.
+      </Text>
+      <Button title='Report a Sharp'/>
+      <Button title='Sharps information'/>
+      
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    fontSize: 24,
+    alignItems: 'center'
+  },
+})
 
