d70e6c3695811842c034cfb612a0033685f94e26
ajc32199 <adam.chiarella@gmail.com>
2025-02-17 15:42:37 -0600

    updated bottom tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -41,6 +41,13 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="info"
+        options={{
+          title: 'Info',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="book.fill" color={color} />,
+        }}
+        />
+      <Tabs.Screen
         name="contact"
         options={{
           title: 'Contact',
