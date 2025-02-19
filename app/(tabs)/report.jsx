9b00d3d84643e551e7d50d8041639b87a89d852e
ajc32199 <adam.chiarella@gmail.com>
2025-02-18 20:58:13 -0600

    added maps, implemented a single marker that moves when a new location is tapped. Additionally, it stores the value of the coordinates in an array and displays it in the console.

--- app/(tabs)/report.jsx
+++ app/(tabs)/report.jsx
@@ -1,45 +1,61 @@
 import { View, Text, StyleSheet, Pressable, TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 import React, { useRef } from 'react'
 import { useNavigation } from 'expo-router'
 import { useEffect } from 'react'
+import * as Location from 'expo-location'
+import { useState } from 'react'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
 
 const INTITIAL_REGION = {
   latitude: 46.788,
   longitude: -92.081,
-  latitudeDelta: 0.5,
-  longitudeDelta: 0.5,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
 }
 
-
 const report = () => {
-  const mapRef = useRef();
-  const navigation = useNavigation();
 
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity onPress={focusMap}>
-          <SafeAreaView style={{ padding: 10 }}>
-            <Text>Focus</Text>
-          </SafeAreaView>
-        </TouchableOpacity>
-      ),
-    });
-  }, [navigation]);
+  //declare an array to hold coordinates of markers as they are created/tapped
+  const [markers, setMarkers] = useState([]);
 
-  const focusMap = () => {
 
+  const handleTap = (e) => {
+    setMarker(e.nativeEvent.coordinate);
+    //add the marker to the array of markers
+    setMarkers([...markers, e.nativeEvent.coordinate]);
+    //log array of markers to console
+    console.log(markers);
   }
 
+  const [marker, setMarker] = useState(null);
+
   return (
+
+    
     <SafeAreaView style={styles.container}>
-      <MapView style={styles.map}
-      initialRegion={INTITIAL_REGION}
-      showsUserLocation
-      showsMyLocationButton
-      />
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        region={INTITIAL_REGION}
+        onPress={handleTap}
+        >
+        <Marker
+          coordinate={{ latitude: 46.788, longitude: -92.081 }}
+          title="Test"
+          description="Test"
+        />
+        {
+          marker && (
+            <Marker
+              coordinate = {marker}
+              title="M1"
+              description="M1"
+            />
+          )
+        }
+      </MapView>
     </SafeAreaView>
   )
 }
